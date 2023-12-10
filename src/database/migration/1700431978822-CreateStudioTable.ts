8309743b26a656337b9897908b813eabf3eaa121
Elivelton Mendes <eliveltonmendes14@gmail.com>
2023-12-10 17:48:14 -0300

    chore: lint

--- src/database/migration/1700431978822-CreateStudioTable.ts
+++ src/database/migration/1700431978822-CreateStudioTable.ts
@@ -1,15 +1,14 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateStudioTable1700431978822 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`create table if not exists studio (
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`create table if not exists studio (
             id integer primary key AUTOINCREMENT,
             name character varying not null
         )`);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`drop table studio`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop table studio`);
+  }
 }
--- src/database/migration/1702062590487-CreateMovieStudioTable.ts
+++ src/database/migration/1702062590487-CreateMovieStudioTable.ts
@@ -1,18 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateMovieStudioTable1702062590487 implements MigrationInterface {
-
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`create table movie_studio (
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`create table movie_studio (
             id integer primary key AUTOINCREMENT,
             movie_id integer not null,
             studio_id integer not null,
             foreign key (movie_id) references movie (id),
             foreign key (studio_id) references studio (id)
         )`);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`drop table movie_studio`);
-    }
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop table movie_studio`);
+  }
 }
