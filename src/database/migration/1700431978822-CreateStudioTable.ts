746e3df48695ba876cfb386041e8158182e6221f
Elivelton Mendes <eliveltonmendes14@gmail.com>
2023-12-10 17:39:40 -0300

    feat: create migrations

---
+++ src/database/migration/1700431978822-CreateStudioTable.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateStudioTable1700431978822 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`create table if not exists studio (
+            id integer primary key AUTOINCREMENT,
+            name character varying not null
+        )`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`drop table studio`);
+    }
+}
